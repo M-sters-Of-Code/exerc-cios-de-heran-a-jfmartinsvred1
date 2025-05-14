535637c5022eea413a8c54370d113c1eadc42315
jfmartinsvred1 <jfmartinsvred@gmail.com>
2025-05-14 20:31:13 -0300

    ajustes

---
+++ SistemaBancario/Model/ContaCaixinha.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Model
+{
+    public class ContaCaixinha : Conta
+    {
+        public override void Depositar(decimal valor)
+        {
+
+            if (valor <= 1)
+                throw new ArgumentOutOfRangeException("O valor do depósito deve ser maior ou igual a 1.");
+            var valorComLucro = valor * 1.01m;
+            this.Saldo += valorComLucro;
+        }
+
+        public override void Sacar(decimal valor)
+        {
+            base.Sacar(valor);
+        }
+        public ContaCaixinha(decimal saldo) : base(saldo)
+        {
+        }
+
+        public ContaCaixinha(Cliente cliente) : base(cliente)
+        {
+        }
+    }
+}
